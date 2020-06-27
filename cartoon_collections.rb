42bfeb996fca8de25f9e0589dadb11ab62a67258
Jessie Donovan <donovanajessie@gmail.com>
2020-06-27 02:58:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,9 @@ def long_planeteer_calls(planeteer_calls)
 end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
+array.select do {|cheese| cheese == "cheddar" || "gouda" || "camembert"}
+  
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
